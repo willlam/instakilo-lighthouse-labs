54d0a1c867450faed9dfba00c85396f6f23b9e0b
will <will@willlam.net>
2016-01-27 11:50:36 -0500

    Initial Commit

---
+++ instakilo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  instakilo
+//
+//  Created by William Lam on 2016-01-27.
+//  Copyright © 2016 William Lam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ instakilo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  instakilo
+//
+//  Created by William Lam on 2016-01-27.
+//  Copyright © 2016 William Lam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
